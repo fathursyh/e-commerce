cbcada44507d6cf99806af35c5018d7ec2a701a6
Fathursyah Awandanu <fathur.syah1@gmail.com>
2025-01-15 02:59:04 +0700

    first commit

---
+++ src/components/WelcomeHome.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-red-500">this is from Vue component {{ reactive }}</h1>
+  <p>{{ fromAstro }}</p>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const reactive = ref('patur');
+
+  defineProps({
+    fromAstro: {type: String, required: false}
+  })
+</script>
