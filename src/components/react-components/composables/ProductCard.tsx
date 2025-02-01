387ccf9971fdd747820c5e73e719012132517a72
Fathursyah Awandanu <fathur.syah1@gmail.com>
2025-02-01 17:48:59 +0700

    update : add buy functionality in feature items

--- src/components/react-components/composables/ProductCard.tsx
+++ src/components/react-components/composables/ProductCard.tsx
@@ -7,7 +7,9 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import type { ProductType } from "src/models/productType";
+import { insertProduct } from "src/stores/app-store";
 import { localCurency } from "src/stores/utility";
+import SimpleToast from "./SimpleToast";
 
 interface CardProps {
   className?: string,
@@ -28,7 +30,11 @@ export default function ProductCard({ className, data} : CardProps) {
       </a>
       <CardContent className="pb-0">
         <div className="grid grid-rows-2 gap-2">
-          <Button variant={'primary'} className="text-white">Buy</Button>
+        <SimpleToast text="Produk berhasil ditambahkan!" >
+          <Button variant={'primary'} className="text-white w-full" onClick={()=>{
+            insertProduct({id: '3', quantity: 1})
+          }}>Buy</Button>
+          </SimpleToast>
         <p className="text-center text-gray-500 text-sm">Stock : <span className="text-blue-500 font-medium">{data?.stock}</span></p>
         </div>
       </CardContent>
