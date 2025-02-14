4f3990c64ed2fcbb451d72da51d93d4a1fa07442
fathursyh <fathur.syah1@gmail.com>
2025-02-14 21:29:10 +0700

    update : add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,18 @@ import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   trailingSlash: 'ignore',
   output: 'server',
+
   security: {
     checkOrigin: true
   },
-  integrations: [vue(), tailwind(), react() ]
+
+  integrations: [vue(), tailwind(), react() ],
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
